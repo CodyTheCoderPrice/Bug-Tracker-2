5584e96dee05db2bb2da3351f626320819bd392c
Cody Price <CodyWilliamPrice@outlook.com>
2024-05-03 12:02:28 -0400

    Created Postgres db schema

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
---
+++ server/schema.sql
@@ -0,0 +1,84 @@
+DROP DATABASE IF EXISTS bugtracker;
+
+CREATE DATABASE bugtracker;
+
+CREATE TABLE
+  account (
+    account_id SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE,
+    hash_pass VARCHAR(255),
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    join_date DATE,
+    last_edited_timestamp BIGINT
+  );
+
+CREATE TABLE
+  project (
+    project_id SERIAL PRIMARY KEY,
+    account_id INTEGER,
+    name VARCHAR(255),
+    description TEXT,
+    creation_date DATE,
+    last_edited_timestamp BIGINT,
+    CONSTRAINT fk_account FOREIGN KEY (account_id) REFERENCES account (account_id)
+  );
+
+CREATE TABLE
+  priority (
+    priority_id SERIAL PRIMARY KEY,
+    name TEXT,
+    order_number SMALLINT
+  );
+
+INSERT INTO
+  priority (name, order_number)
+VALUES
+  ('Low', 0),
+  ('Medium', 1),
+  ('High', 2);
+
+CREATE TABLE
+  status (
+    status_id SERIAL PRIMARY KEY,
+    name TEXT,
+    order_number SMALLINT,
+    color TEXT,
+    marks_completed BOOLEAN DEFAULT false
+  );
+
+INSERT INTO
+  status (name, order_number, color, marks_completed)
+VALUES
+  ('Open', 0, 'blue', false),
+  ('In Progress', 1, 'purple', false),
+  ('Testing', 2, 'orange', false),
+  ('Closed', 3, 'green', true);
+
+CREATE TABLE
+  bug (
+    bug_id SERIAL PRIMARY KEY,
+    project_id INTEGER,
+    name VARCHAR(255),
+    description TEXT,
+    location TEXT,
+    priority_id SMALLINT NOT NULL,
+    status_id SMALLINT NOT NULL,
+    creation_date DATE,
+    due_date DATE,
+    completion_date DATE,
+    last_edited_timestamp BIGINT,
+    CONSTRAINT fk_project FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE,
+    CONSTRAINT fk_priority FOREIGN KEY (priority_id) REFERENCES priority (priority_id) ON DELETE SET NULL,
+    CONSTRAINT fk_status FOREIGN KEY (status_id) REFERENCES status (status_id) ON DELETE SET NULL
+  );
+
+CREATE TABLE
+  comment (
+    comment_id SERIAL PRIMARY KEY,
+    bug_id INTEGER,
+    description TEXT,
+    creation_date DATE,
+    last_edited_timestamp BIGINT,
+    CONSTRAINT fk_bug FOREIGN KEY (bug_id) REFERENCES bug (bug_id) ON DELETE CASCADE
+  );
