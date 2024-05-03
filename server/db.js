153c82243a8c52df49e3f244fe46630dc8e0bea0
Cody Price <CodyWilliamPrice@outlook.com>
2024-05-03 16:56:50 -0400

    Set up db file

---
+++ server/db.js
@@ -0,0 +1,27 @@
+import pg from 'pg';
+const { Pool } = pg;
+
+const env = process.env.NODE_ENV || 'development';
+
+// Connection string for Postgres database hosted on Heroku
+let connectionString = {
+	connectionString: process.env.DATABASE_URL,
+	ssl: {
+		rejectUnauthorized: false,
+	},
+};
+
+// Connection string for running locally
+if (env === 'development') {
+	connectionString = {
+		user: 'postgres',
+		password: 'PostgresEasyPassword',
+		host: 'localhost',
+		port: 5432,
+		database: 'bugtracker',
+	};
+}
+
+const pool = new Pool(connectionString);
+
+module.exports = pool;
