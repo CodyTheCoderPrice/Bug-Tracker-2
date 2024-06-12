fbf4ecea4b2c85b8d44714db6b57aac183c621ef
Cody Price <CodyWilliamPrice@outlook.com>
2024-06-12 09:50:19 -0400

    Fixed bug where having no bugs or comments caused crash

--- client/src/features/bugs/CreateBug.tsx
+++ client/src/features/bugs/CreateBug.tsx
@@ -8,7 +8,7 @@ function CreateBug() {
 	const { projects } = useAppSelector((state) => state.projects);
 
 	const [bugInfo, setBugInfo] = useState({
-		project_id: projects ? projects[0].project_id : -1,
+		project_id: projects && projects.length > 0 ? projects[0].project_id : -1,
 		name: '',
 		description: '',
 		location: '',
@@ -37,111 +37,119 @@ function CreateBug() {
 	return (
 		<>
 			<h1>Create Bug</h1>
-			<form noValidate autoComplete='off' onSubmit={handleSubmit}>
-				<select
-					name='project_id'
-					onChange={handleInput}
-					defaultValue={bugInfo.project_id}
-					style={{ display: 'block' }}
-				>
-					{projects?.map((project, idx) => {
-						return (
-							<option key={idx} value={project.project_id}>
-								{project.name}
-							</option>
-						);
-					})}
-				</select>
-				<input
-					type='text'
-					name='name'
-					placeholder='Bug name'
-					onChange={handleInput}
-					value={bugInfo.name}
-					style={{ display: 'block', width: '227px' }}
-				/>
-				<textarea
-					name='description'
-					placeholder='Description'
-					onChange={handleInput}
-					value={bugInfo.description}
-					rows={4}
-					cols={30}
-					style={{ display: 'block' }}
-				/>
-				<input
-					type='text'
-					name='location'
-					placeholder='Location'
-					onChange={handleInput}
-					value={bugInfo.location}
-					style={{ display: 'block', width: '227px' }}
-				/>
-				<select
-					name='priority_id'
-					onChange={handleInput}
-					defaultValue={bugInfo.priority_id}
-					style={{ display: 'block' }}
-				>
-					<option value={1}>Low</option>
-					<option value={2}>Medium</option>
-					<option value={3}>High</option>
-				</select>
-				<select
-					name='status_id'
-					onChange={handleInput}
-					defaultValue={bugInfo.status_id}
-					style={{ display: 'block' }}
-				>
-					<option value={1}>Open</option>
-					<option value={2}>In Progress</option>
-					<option value={3}>Testing</option>
-					<option value={4}>Closed</option>
-				</select>
-				<input
-					type='date'
-					name='due_date'
-					onChange={handleInput}
-					value={bugInfo.due_date !== null ? bugInfo.due_date : ''}
-					style={{ display: 'block' }}
-				/>
-				<input
-					type='date'
-					name='complete_date'
-					onChange={handleInput}
-					value={bugInfo.complete_date !== null ? bugInfo.complete_date : ''}
-					style={{ display: 'block' }}
-				/>
-				<button type='submit'>Create</button>
-			</form>
-			{createBugLoading && <h3>Loading...</h3>}
-			{createBugSuccess && <p>Bug Created</p>}
-			{createBugErrors?.project_id && (
-				<p style={{ color: 'red' }}>{createBugErrors.project_id}</p>
-			)}
-			{createBugErrors?.name && (
-				<p style={{ color: 'red' }}>{createBugErrors.name}</p>
-			)}
-			{createBugErrors?.description && (
-				<p style={{ color: 'red' }}>{createBugErrors.description}</p>
-			)}
-			{createBugErrors?.location && (
-				<p style={{ color: 'red' }}>{createBugErrors.location}</p>
-			)}
-			{createBugErrors?.priority_id && (
-				<p style={{ color: 'red' }}>{createBugErrors.priority_id}</p>
-			)}
-			{createBugErrors?.status_id && (
-				<p style={{ color: 'red' }}>{createBugErrors.status_id}</p>
-			)}
-			{createBugErrors?.due_date && (
-				<p style={{ color: 'red' }}>{createBugErrors.due_date}</p>
-			)}
-			{createBugErrors?.complete_date && (
-				<p style={{ color: 'red' }}>{createBugErrors.complete_date}</p>
-			)}
-			{createBugErrors?.server && (
-				<p style={{ color: 'red' }}>{createBugErrors.server}</p>
+			{projects?.length === 0 ? (
+				<p>No projects</p>
+			) : (
+				<>
+					<form noValidate autoComplete='off' onSubmit={handleSubmit}>
+						<select
+							name='project_id'
+							onChange={handleInput}
+							defaultValue={bugInfo.project_id}
+							style={{ display: 'block' }}
+						>
+							{projects?.map((project, idx) => {
+								return (
+									<option key={idx} value={project.project_id}>
+										{project.name}
+									</option>
+								);
+							})}
+						</select>
+						<input
+							type='text'
+							name='name'
+							placeholder='Bug name'
+							onChange={handleInput}
+							value={bugInfo.name}
+							style={{ display: 'block', width: '227px' }}
+						/>
+						<textarea
+							name='description'
+							placeholder='Description'
+							onChange={handleInput}
+							value={bugInfo.description}
+							rows={4}
+							cols={30}
+							style={{ display: 'block' }}
+						/>
+						<input
+							type='text'
+							name='location'
+							placeholder='Location'
+							onChange={handleInput}
+							value={bugInfo.location}
+							style={{ display: 'block', width: '227px' }}
+						/>
+						<select
+							name='priority_id'
+							onChange={handleInput}
+							defaultValue={bugInfo.priority_id}
+							style={{ display: 'block' }}
+						>
+							<option value={1}>Low</option>
+							<option value={2}>Medium</option>
+							<option value={3}>High</option>
+						</select>
+						<select
+							name='status_id'
+							onChange={handleInput}
+							defaultValue={bugInfo.status_id}
+							style={{ display: 'block' }}
+						>
+							<option value={1}>Open</option>
+							<option value={2}>In Progress</option>
+							<option value={3}>Testing</option>
+							<option value={4}>Closed</option>
+						</select>
+						<input
+							type='date'
+							name='due_date'
+							onChange={handleInput}
+							value={bugInfo.due_date !== null ? bugInfo.due_date : ''}
+							style={{ display: 'block' }}
+						/>
+						<input
+							type='date'
+							name='complete_date'
+							onChange={handleInput}
+							value={
+								bugInfo.complete_date !== null ? bugInfo.complete_date : ''
+							}
+							style={{ display: 'block' }}
+						/>
+						<button type='submit'>Create</button>
+					</form>
+					{createBugLoading && <h3>Loading...</h3>}
+					{createBugSuccess && <p>Bug Created</p>}
+					{createBugErrors?.project_id && (
+						<p style={{ color: 'red' }}>{createBugErrors.project_id}</p>
+					)}
+					{createBugErrors?.name && (
+						<p style={{ color: 'red' }}>{createBugErrors.name}</p>
+					)}
+					{createBugErrors?.description && (
+						<p style={{ color: 'red' }}>{createBugErrors.description}</p>
+					)}
+					{createBugErrors?.location && (
+						<p style={{ color: 'red' }}>{createBugErrors.location}</p>
+					)}
+					{createBugErrors?.priority_id && (
+						<p style={{ color: 'red' }}>{createBugErrors.priority_id}</p>
+					)}
+					{createBugErrors?.status_id && (
+						<p style={{ color: 'red' }}>{createBugErrors.status_id}</p>
+					)}
+					{createBugErrors?.due_date && (
+						<p style={{ color: 'red' }}>{createBugErrors.due_date}</p>
+					)}
+					{createBugErrors?.complete_date && (
+						<p style={{ color: 'red' }}>{createBugErrors.complete_date}</p>
+					)}
+					{createBugErrors?.server && (
+						<p style={{ color: 'red' }}>{createBugErrors.server}</p>
+					)}
+				</>
 			)}
 		</>
 	);
--- client/src/features/comments/CreateComment.tsx
+++ client/src/features/comments/CreateComment.tsx
@@ -8,7 +8,7 @@ function CreateComment() {
 	const { bugs } = useAppSelector((state) => state.bugs);
 
 	const [commentInfo, setCommentInfo] = useState({
-		bug_id: bugs ? bugs[0].bug_id : -1,
+		bug_id: bugs && bugs.length > 0 ? bugs[0].bug_id : -1,
 		description: '',
 	});
 
@@ -31,42 +31,48 @@ function CreateComment() {
 	return (
 		<>
 			<h1>Create Comment</h1>
-			<form noValidate autoComplete='off' onSubmit={handleSubmit}>
-				<select
-					name='bug_id'
-					onChange={handleInput}
-					defaultValue={commentInfo.bug_id}
-					style={{ display: 'block' }}
-				>
-					{bugs?.map((bug, idx) => {
-						return (
-							<option key={idx} value={bug.bug_id}>
-								{bug.name}
-							</option>
-						);
-					})}
-				</select>
-				<textarea
-					name='description'
-					placeholder='Description'
-					onChange={handleInput}
-					value={commentInfo.description}
-					rows={4}
-					cols={30}
-					style={{ display: 'block' }}
-				/>
-				<button type='submit'>Create</button>
-			</form>
-			{createCommentLoading && <h3>Loading...</h3>}
-			{createCommentSuccess && <p>Comment Created</p>}
-			{createCommentErrors?.bug_id && (
-				<p style={{ color: 'red' }}>{createCommentErrors.bug_id}</p>
-			)}
-			{createCommentErrors?.description && (
-				<p style={{ color: 'red' }}>{createCommentErrors.description}</p>
-			)}
-			{createCommentErrors?.server && (
-				<p style={{ color: 'red' }}>{createCommentErrors.server}</p>
+			{bugs?.length === 0 ? (
+				<p>No bugs</p>
+			) : (
+				<>
+					<form noValidate autoComplete='off' onSubmit={handleSubmit}>
+						<select
+							name='bug_id'
+							onChange={handleInput}
+							defaultValue={commentInfo.bug_id}
+							style={{ display: 'block' }}
+						>
+							{bugs?.map((bug, idx) => {
+								return (
+									<option key={idx} value={bug.bug_id}>
+										{bug.name}
+									</option>
+								);
+							})}
+						</select>
+						<textarea
+							name='description'
+							placeholder='Description'
+							onChange={handleInput}
+							value={commentInfo.description}
+							rows={4}
+							cols={30}
+							style={{ display: 'block' }}
+						/>
+						<button type='submit'>Create</button>
+					</form>
+					{createCommentLoading && <h3>Loading...</h3>}
+					{createCommentSuccess && <p>Comment Created</p>}
+					{createCommentErrors?.bug_id && (
+						<p style={{ color: 'red' }}>{createCommentErrors.bug_id}</p>
+					)}
+					{createCommentErrors?.description && (
+						<p style={{ color: 'red' }}>{createCommentErrors.description}</p>
+					)}
+					{createCommentErrors?.server && (
+						<p style={{ color: 'red' }}>{createCommentErrors.server}</p>
+					)}
+				</>
 			)}
 		</>
 	);
