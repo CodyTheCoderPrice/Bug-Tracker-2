3985dd3ae23dd2c4d1048dbf2aeb45eeea486a9f
Cody Price <CodyWilliamPrice@outlook.com>
2024-06-19 12:06:24 -0400

    Added filter buttons and began working on tables

--- client/src/features/bugs/bugSlice.ts
+++ client/src/features/bugs/bugSlice.ts
@@ -1,234 +1,234 @@
-import { PayloadAction, createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import axiosInstance from '@/API';
-import { login, relogin } from '../auth/authSlice';
-import { deleteProject } from '../projects/projectSlice';
+import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axiosInstance from "@/API";
+import { login, relogin } from "../auth/authSlice";
+import { deleteProject } from "../projects/projectSlice";
 
-type TBug = {
-	bug_id: number;
-	project_id: number;
-	account_id: number;
-	name: string;
-	description: string;
-	priority_id: number;
-	priority_name: string;
-	status_id: number;
-	status_name: string;
-	create_time: string;
-	due_date: string | null;
-	complete_date: string | null;
-	update_time: string;
+export type TBug = {
+  bug_id: number;
+  project_id: number;
+  account_id: number;
+  name: string;
+  description: string;
+  priority_id: number;
+  priority_name: string;
+  status_id: number;
+  status_name: string;
+  create_time: string;
+  due_date: string | null;
+  complete_date: string | null;
+  update_time: string;
 };
 
 type TCreateBugError = {
-	project_id: string | undefined;
-	name: string | undefined;
-	description: string | undefined;
-	priority_id: string | undefined;
-	status_id: string | undefined;
-	due_date: string | undefined;
-	complete_date: string | undefined;
-	server: string | undefined;
+  project_id: string | undefined;
+  name: string | undefined;
+  description: string | undefined;
+  priority_id: string | undefined;
+  status_id: string | undefined;
+  due_date: string | undefined;
+  complete_date: string | undefined;
+  server: string | undefined;
 };
 
 type TUpdateBugError = {
-	bug_id: string | undefined;
-	project_id: string | undefined;
-	name: string | undefined;
-	description: string | undefined;
-	priority_id: string | undefined;
-	status_id: string | undefined;
-	due_date: string | undefined;
-	complete_date: string | undefined;
-	server: string | undefined;
+  bug_id: string | undefined;
+  project_id: string | undefined;
+  name: string | undefined;
+  description: string | undefined;
+  priority_id: string | undefined;
+  status_id: string | undefined;
+  due_date: string | undefined;
+  complete_date: string | undefined;
+  server: string | undefined;
 };
 
 type TDeleteBugError = {
-	bug_id: string | undefined;
-	server: string | undefined;
+  bug_id: string | undefined;
+  server: string | undefined;
 };
 
 type TInitialState = {
-	createBugLoading: boolean;
-	updateBugLoading: boolean;
-	deleteBugLoading: boolean;
-	bugs: TBug[] | null;
-	createBugSuccess: boolean;
-	updateBugSuccess: boolean;
-	deleteBugSuccess: boolean;
-	createBugErrors: TCreateBugError | null;
-	updateBugErrors: TUpdateBugError | null;
-	deleteBugErrors: TDeleteBugError | null;
+  createBugLoading: boolean;
+  updateBugLoading: boolean;
+  deleteBugLoading: boolean;
+  bugs: TBug[] | null;
+  createBugSuccess: boolean;
+  updateBugSuccess: boolean;
+  deleteBugSuccess: boolean;
+  createBugErrors: TCreateBugError | null;
+  updateBugErrors: TUpdateBugError | null;
+  deleteBugErrors: TDeleteBugError | null;
 };
 
 const initialState: TInitialState = {
-	createBugLoading: false,
-	updateBugLoading: false,
-	deleteBugLoading: false,
-	bugs: null,
-	createBugSuccess: false,
-	updateBugSuccess: false,
-	deleteBugSuccess: false,
-	createBugErrors: null,
-	updateBugErrors: null,
-	deleteBugErrors: null,
+  createBugLoading: false,
+  updateBugLoading: false,
+  deleteBugLoading: false,
+  bugs: null,
+  createBugSuccess: false,
+  updateBugSuccess: false,
+  deleteBugSuccess: false,
+  createBugErrors: null,
+  updateBugErrors: null,
+  deleteBugErrors: null,
 };
 
 export const createBug = createAsyncThunk(
-	'bugs/create',
-	async (
-		bugInfo: {
-			project_id: number;
-			name: string;
-			description: string;
-			priority_id: number;
-			status_id: number;
-			due_date: string | null;
-			complete_date: string | null;
-		},
-		{ rejectWithValue }
-	) => {
-		try {
-			const response = await axiosInstance.post('/api/v1/bugs/create', bugInfo);
-			return response.data;
-		} catch (err: any) {
-			if (!err.response.data.errors) {
-				return rejectWithValue(null);
-			}
-			return rejectWithValue(err.response.data.errors);
-		}
-	}
+  "bugs/create",
+  async (
+    bugInfo: {
+      project_id: number;
+      name: string;
+      description: string;
+      priority_id: number;
+      status_id: number;
+      due_date: string | null;
+      complete_date: string | null;
+    },
+    { rejectWithValue },
+  ) => {
+    try {
+      const response = await axiosInstance.post("/api/v1/bugs/create", bugInfo);
+      return response.data;
+    } catch (err: any) {
+      if (!err.response.data.errors) {
+        return rejectWithValue(null);
+      }
+      return rejectWithValue(err.response.data.errors);
+    }
+  },
 );
 
 export const updateBug = createAsyncThunk(
-	'bugs/update',
-	async (
-		bugInfo: {
-			bug_id: number;
-			project_id: number;
-			name: string;
-			description: string;
-			priority_id: number;
-			status_id: number;
-			due_date: string | null;
-			complete_date: string | null;
-		},
-		{ rejectWithValue }
-	) => {
-		try {
-			const response = await axiosInstance.put('/api/v1/bugs/update', bugInfo);
-			return response.data;
-		} catch (err: any) {
-			if (!err.response.data.errors) {
-				return rejectWithValue(null);
-			}
-			return rejectWithValue(err.response.data.errors);
-		}
-	}
+  "bugs/update",
+  async (
+    bugInfo: {
+      bug_id: number;
+      project_id: number;
+      name: string;
+      description: string;
+      priority_id: number;
+      status_id: number;
+      due_date: string | null;
+      complete_date: string | null;
+    },
+    { rejectWithValue },
+  ) => {
+    try {
+      const response = await axiosInstance.put("/api/v1/bugs/update", bugInfo);
+      return response.data;
+    } catch (err: any) {
+      if (!err.response.data.errors) {
+        return rejectWithValue(null);
+      }
+      return rejectWithValue(err.response.data.errors);
+    }
+  },
 );
 
 export const deleteBug = createAsyncThunk(
-	'bugs/delete',
-	async (bugInfo: { bug_id: number }, { rejectWithValue }) => {
-		try {
-			const response = await axiosInstance.delete('/api/v1/bugs/delete', {
-				data: bugInfo,
-			});
-			return response.data;
-		} catch (err: any) {
-			if (!err.response.data.errors) {
-				return rejectWithValue(null);
-			}
-			return rejectWithValue(err.response.data.errors);
-		}
-	}
+  "bugs/delete",
+  async (bugInfo: { bug_id: number }, { rejectWithValue }) => {
+    try {
+      const response = await axiosInstance.delete("/api/v1/bugs/delete", {
+        data: bugInfo,
+      });
+      return response.data;
+    } catch (err: any) {
+      if (!err.response.data.errors) {
+        return rejectWithValue(null);
+      }
+      return rejectWithValue(err.response.data.errors);
+    }
+  },
 );
 
 const bugSlice = createSlice({
-	name: 'bugs',
-	initialState,
-	reducers: {},
-	extraReducers: (builder) => {
-		// Login / Relogin
-		builder.addCase(
-			login.fulfilled,
-			(state, action: PayloadAction<{ bugs: TBug[] }>) => {
-				state.bugs = action.payload.bugs;
-			}
-		);
-		builder.addCase(
-			relogin.fulfilled,
-			(state, action: PayloadAction<{ bugs: TBug[] }>) => {
-				state.bugs = action.payload.bugs;
-			}
-		);
-		// delete project
-		builder.addCase(
-			deleteProject.fulfilled,
-			(state, action: PayloadAction<{ bugs: TBug[] }>) => {
-				state.bugs = action.payload.bugs;
-			}
-		);
-		// Create
-		builder.addCase(createBug.pending, (state) => {
-			state.createBugLoading = true;
-			state.createBugSuccess = false;
-			state.createBugErrors = null;
-		});
-		builder.addCase(
-			createBug.fulfilled,
-			(state, action: PayloadAction<{ bugs: TBug[] }>) => {
-				state.createBugLoading = false;
-				state.bugs = action.payload.bugs;
-				state.createBugSuccess = true;
-				state.createBugErrors = null;
-			}
-		);
-		builder.addCase(createBug.rejected, (state, action: any) => {
-			state.createBugLoading = false;
-			state.createBugSuccess = false;
-			state.createBugErrors = action.payload;
-		});
-		// Update
-		builder.addCase(updateBug.pending, (state) => {
-			state.updateBugLoading = true;
-			state.updateBugSuccess = false;
-			state.updateBugErrors = null;
-		});
-		builder.addCase(
-			updateBug.fulfilled,
-			(state, action: PayloadAction<{ bugs: TBug[] }>) => {
-				state.updateBugLoading = false;
-				state.bugs = action.payload.bugs;
-				state.updateBugSuccess = true;
-				state.updateBugErrors = null;
-			}
-		);
-		builder.addCase(updateBug.rejected, (state, action: any) => {
-			state.updateBugLoading = false;
-			state.updateBugSuccess = false;
-			state.updateBugErrors = action.payload;
-		});
-		// delete
-		builder.addCase(deleteBug.pending, (state) => {
-			state.deleteBugLoading = true;
-			state.deleteBugSuccess = false;
-			state.deleteBugErrors = null;
-		});
-		builder.addCase(
-			deleteBug.fulfilled,
-			(state, action: PayloadAction<{ bugs: TBug[] }>) => {
-				state.deleteBugLoading = false;
-				state.bugs = action.payload.bugs;
-				state.deleteBugSuccess = true;
-				state.deleteBugErrors = null;
-			}
-		);
-		builder.addCase(deleteBug.rejected, (state, action: any) => {
-			state.deleteBugLoading = false;
-			state.deleteBugSuccess = false;
-			state.deleteBugErrors = action.payload;
-		});
-	},
+  name: "bugs",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    // Login / Relogin
+    builder.addCase(
+      login.fulfilled,
+      (state, action: PayloadAction<{ bugs: TBug[] }>) => {
+        state.bugs = action.payload.bugs;
+      },
+    );
+    builder.addCase(
+      relogin.fulfilled,
+      (state, action: PayloadAction<{ bugs: TBug[] }>) => {
+        state.bugs = action.payload.bugs;
+      },
+    );
+    // delete project
+    builder.addCase(
+      deleteProject.fulfilled,
+      (state, action: PayloadAction<{ bugs: TBug[] }>) => {
+        state.bugs = action.payload.bugs;
+      },
+    );
+    // Create
+    builder.addCase(createBug.pending, (state) => {
+      state.createBugLoading = true;
+      state.createBugSuccess = false;
+      state.createBugErrors = null;
+    });
+    builder.addCase(
+      createBug.fulfilled,
+      (state, action: PayloadAction<{ bugs: TBug[] }>) => {
+        state.createBugLoading = false;
+        state.bugs = action.payload.bugs;
+        state.createBugSuccess = true;
+        state.createBugErrors = null;
+      },
+    );
+    builder.addCase(createBug.rejected, (state, action: any) => {
+      state.createBugLoading = false;
+      state.createBugSuccess = false;
+      state.createBugErrors = action.payload;
+    });
+    // Update
+    builder.addCase(updateBug.pending, (state) => {
+      state.updateBugLoading = true;
+      state.updateBugSuccess = false;
+      state.updateBugErrors = null;
+    });
+    builder.addCase(
+      updateBug.fulfilled,
+      (state, action: PayloadAction<{ bugs: TBug[] }>) => {
+        state.updateBugLoading = false;
+        state.bugs = action.payload.bugs;
+        state.updateBugSuccess = true;
+        state.updateBugErrors = null;
+      },
+    );
+    builder.addCase(updateBug.rejected, (state, action: any) => {
+      state.updateBugLoading = false;
+      state.updateBugSuccess = false;
+      state.updateBugErrors = action.payload;
+    });
+    // delete
+    builder.addCase(deleteBug.pending, (state) => {
+      state.deleteBugLoading = true;
+      state.deleteBugSuccess = false;
+      state.deleteBugErrors = null;
+    });
+    builder.addCase(
+      deleteBug.fulfilled,
+      (state, action: PayloadAction<{ bugs: TBug[] }>) => {
+        state.deleteBugLoading = false;
+        state.bugs = action.payload.bugs;
+        state.deleteBugSuccess = true;
+        state.deleteBugErrors = null;
+      },
+    );
+    builder.addCase(deleteBug.rejected, (state, action: any) => {
+      state.deleteBugLoading = false;
+      state.deleteBugSuccess = false;
+      state.deleteBugErrors = action.payload;
+    });
+  },
 });
 
 export default bugSlice.reducer;
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,30 +1,103 @@
-import { MouseEventHandler } from "react";
+import { MouseEventHandler, useState } from "react";
+import { TBug } from "@/features/bugs/bugSlice";
 import openBugIcon from "@/assets/icons/icon_bug_open.svg";
 import inProgressBugIcon from "@/assets/icons/icon_bug_progress.svg";
 import inTestingBugIcon from "@/assets/icons/icon_bug_testing.svg";
 import closedBugIcon from "@/assets/icons/icon_bug_closed.svg";
-
-const bugStatusButton = (
-  src: string,
-  title: string,
-  amount: number,
-  onClick: MouseEventHandler<HTMLButtonElement>,
-) => {
-  return (
-    <button
-      onClick={onClick}
-      className="mr-[50px] flex w-[225px] items-center rounded-lg bg-plain-1 px-4 shadow hover:shadow-lg"
-    >
-      <img src={src} alt={`icon for ${title}`} className="h-[50px] rounded" />
-      <div className="ml-3 flex flex-col text-left">
-        <span>{title}</span>
-        <span className="text-xl">{amount}</span>
-      </div>
-    </button>
-  );
-};
+import { useAppSelector } from "@/app/hooks";
 
 function HomePage() {
+  type TFilter = 0 | 1 | 2;
+  const [dueFilterSelected, setDueFilterSelected] = useState<TFilter>(0);
+  const [overdueFilterSelected, setOverdueFilterSelected] =
+    useState<TFilter>(0);
+
+  const bugStatusButton = (
+    src: string,
+    title: string,
+    amount: number,
+    onClick: MouseEventHandler<HTMLButtonElement>,
+  ) => {
+    return (
+      <button
+        onClick={onClick}
+        className="mr-[50px] flex w-[225px] items-center rounded-lg bg-plain-1 px-4 shadow hover:shadow-lg"
+      >
+        <img src={src} alt={`icon for ${title}`} className="h-[50px] rounded" />
+        <div className="ml-3 flex flex-col text-left">
+          <span>{title}</span>
+          <span className="text-xl">{amount}</span>
+        </div>
+      </button>
+    );
+  };
+
+  const titleHeader = (title: string) => {
+    return <h2 className="text-xl font-semibold">{title}</h2>;
+  };
+
+  const filterButtons = (
+    btn1Text: string,
+    btn1OnClick: MouseEventHandler<HTMLButtonElement>,
+    btn2Text: string,
+    btn2OnClick: MouseEventHandler<HTMLButtonElement>,
+    btn3Text: string,
+    btn3OnClick: MouseEventHandler<HTMLButtonElement>,
+    forDueSoon: boolean,
+  ) => {
+    const filterSelected = forDueSoon
+      ? dueFilterSelected
+      : overdueFilterSelected;
+
+    return (
+      <div className="mt-6 font-medium text-primary-2">
+        <button
+          onClick={btn1OnClick}
+          className={
+            "rounded-l border border-plain-3 px-4 py-[1px]" +
+            (filterSelected === 0 ? " bg-primary-1 text-white" : "")
+          }
+        >
+          {btn1Text}
+        </button>
+        <button
+          onClick={btn2OnClick}
+          className={
+            "border border-l-0 border-plain-3 px-4 py-[1px]" +
+            (filterSelected === 1 ? " bg-primary-1 text-white" : "")
+          }
+        >
+          {btn2Text}
+        </button>
+        <button
+          onClick={btn3OnClick}
+          className={
+            "rounded-r border border-l-0 border-plain-3 px-4 py-[1px]" +
+            (filterSelected === 2 ? " bg-primary-1 text-white" : "")
+          }
+        >
+          {btn3Text}
+        </button>
+      </div>
+    );
+  };
+
+  const bugTable = (bugList: TBug[] | null) => {
+    return (
+      <table className="mt-4 flex-grow">
+        <thead>
+          <tr className="bg-plain-3">
+            <th>BUG</th>
+            <th>PROJECT</th>
+            <th>DUE DATE</th>
+          </tr>
+        </thead>
+      </table>
+    );
+  };
+
+  const { bugs } = useAppSelector((state) => state.bugs);
+
   return (
     <div className="flex flex-1 flex-col">
       <div className="mx-10 mt-10 flex h-[70px]">
@@ -34,8 +107,44 @@ function HomePage() {
         {bugStatusButton(closedBugIcon, "Closed Bugs", -1, () => {})}
       </div>
       <div className="mx-10 mb-10 mt-6 flex grow">
-        <div className="mr-5 flex-1 rounded bg-plain-1 shadow"></div>
-        <div className="ml-5 flex-1 rounded bg-plain-1 shadow"></div>
+        <div className="mr-5 flex flex-1 flex-col rounded bg-plain-1 p-6 shadow">
+          {titleHeader("Bugs Due Soon")}
+          {filterButtons(
+            "All",
+            () => {
+              setDueFilterSelected(0);
+            },
+            "This Week",
+            () => {
+              setDueFilterSelected(1);
+            },
+            "This Month",
+            () => {
+              setDueFilterSelected(2);
+            },
+            true,
+          )}
+          {bugTable(bugs)}
+        </div>
+        <div className="ml-5 flex flex-1 flex-col rounded bg-plain-1 p-6 shadow">
+          {titleHeader("Overdue Bugs")}
+          {filterButtons(
+            "All",
+            () => {
+              setOverdueFilterSelected(0);
+            },
+            "Past Week",
+            () => {
+              setOverdueFilterSelected(1);
+            },
+            "Past Month",
+            () => {
+              setOverdueFilterSelected(2);
+            },
+            false,
+          )}
+          {bugTable(bugs)}
+        </div>
       </div>
     </div>
   );
